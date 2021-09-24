2772ff8cfda172f252b664c747a2afb68b537392
SkinlessNoodle <78943185+SkinlessNoodle@users.noreply.github.com>
2021-09-24 12:36:09 +1000

    Added Booking.cs. hasnt been scaffholded yet

---
+++ Hotel19760575/Models/Booking.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Hotel19760575.Models
+{
+    public class Booking
+    {
+        // primary key
+        [Key]
+        public int BookingID { get; set; }
+
+        // foreign key
+        public int RoomID { get; set; }
+
+        // foreign key
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        [EmailAddress]
+        public string CustomerEmail { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime CheckIn { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime CheckOut { get; set; }
+
+        public decimal Cost { get; set; }
+        
+    }
+}
