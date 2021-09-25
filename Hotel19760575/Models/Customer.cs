fafdf9d0d74f65c8c1553699d4514aeb4e223c1e
jamsii123 <james.kachappilly27@gmail.com>
2021-09-25 18:28:57 +1000

    new stuff

---
+++ Hotel19760575/Models/Customer.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Hotel19760575.Models
+{
+    public class Customer
+    {
+
+        [Key, Required]
+        [DataType(DataType.EmailAddress)]
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public string Email { get; set; }
+
+        [Required, Display(Name = "Surname")]
+        [RegularExpression(@"[A-Za-z'-]{2,20}")]
+        public string Surname { get; set; }
+
+        [Required, Display(Name = "Given Name")]
+        [RegularExpression(@"[A-Za-z'-]{2,20}")]
+        public string GivenName { get; set; }
+
+        [Required, Display(Name = "Postcode")]
+        [RegularExpression(@"[0-9]{4}")]
+        public string Postcode { get; set; }
+
+        // Navigation Properties
+
+        public ICollection<Booking> TheBookings { get; set; }
+
+    }
+}
