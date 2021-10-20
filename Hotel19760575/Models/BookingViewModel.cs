a466f777856921f907951cf4afc3aefb8283011d
aylinWSU <aylin.tutka@gmail.com>
2021-10-20 23:56:33 +1100

    added Book a Room with James

---
+++ Hotel19760575/Models/BookingViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Hotel19760575.Models
+{
+    public class BookingViewModel
+    {
+        // primary key
+        [Key]
+        public int BookingID { get; set; }
+
+        // foreign key
+        public int RoomID { get; set; }
+
+        // foreign key
+        [Required]
+        [EmailAddress]
+        public string CustomerEmail { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime CheckIn { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime CheckOut { get; set; }
+
+        public decimal Cost { get; set; }
+
+        public Room TheRoom { get; set; }
+        public Customer TheCustomer { get; set; }
+    }
+}
--- Hotel19760575/Pages/Bookings/Create.cshtml.cs
+++ Hotel19760575/Pages/Bookings/Create.cshtml.cs
@@ -5,43 +5,105 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
-using Hotel19760575.Data;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
 using Hotel19760575.Models;
+using System.Security.Claims;
+using System.Diagnostics;
 
 namespace Hotel19760575.Pages.Bookings
+
 {
-    public class CreateModel : PageModel
+    //TO DO: Make sure bookign validation works , convert DateDiff to int
+    public class PeopleDiffModel : PageModel
     {
         private readonly Hotel19760575.Data.ApplicationDbContext _context;
 
-        public CreateModel(Hotel19760575.Data.ApplicationDbContext context)
+        public PeopleDiffModel (Hotel19760575.Data.ApplicationDbContext context)
         {
             _context = context;
         }
 
+        [BindProperty]
+        public BookingViewModel BookingInput { get; set; }
+
+        //public TwoMovieGoers MovieGoersInput { get; set; }
+
+        // List of different movies; for passing to Content file to display
+        public IList<Booking> SameDates { get; set; }
+
+        // GET: MovieGoers/PeopleDiff
         public IActionResult OnGet()
         {
-        ViewData["CustomerEmail"] = new SelectList(_context.Set<Customer>(), "Email", "Email");
-        ViewData["RoomID"] = new SelectList(_context.Room, "ID", "ID");
+            // Get the options for the MovieGoer select list from the database
+            // and save them in ViewData for passing to Content file
+            //ViewData["CustomerEmail"] = new SelectList(_context.Set<Customer>(), "Email", "Email");
+            ViewData["RoomID"] = new SelectList(_context.Room, "ID", "ID");
             return Page();
         }
 
-        [BindProperty]
-        public Booking Booking { get; set; }
-
-        // To protect from overposting attacks, enable the specific properties you want to bind to, for
-        // more details, see https://aka.ms/RazorPagesCRUD.
         public async Task<IActionResult> OnPostAsync()
         {
-            if (!ModelState.IsValid)
-            {
-                return Page();
-            }
 
-            _context.Booking.Add(Booking);
+            string _email = User.FindFirst(ClaimTypes.Name).Value;
+
+            //Needed to retrieve customer email.
+            Customer customer = await _context.Customer.FirstOrDefaultAsync(m => m.Email == _email);
+
+            // prepare the parameters to be inserted into the query
+            var CustomerCheckIn = new SqliteParameter("CheckIn", BookingInput.CheckIn);
+            var CustomerCheckOut = new SqliteParameter("CheckOut", BookingInput.CheckOut);
+            
+
+            // Construct the query to get the movies watched by Moviegoer A but not Moviegoer B
+            // Use placeholders as the parameters
+            var sameDates = _context.Booking.FromSqlRaw("SELECT * FROM Booking INNER JOIN Room ON Booking.RoomID = Room.ID "
+                + "WHERE @CheckIn < Booking.CheckOut AND Booking.CheckIn < @CheckOut", CustomerCheckIn, CustomerCheckOut);
+
+            
+
+            /* "select [Movie].* from [Movie] inner join [Order] on "
+                              + "[Movie].ID = [Order].MovieID where [Order].MovieGoerEmail = @CheckIn and "
+                              + "[Movie].ID not in (select [Movie].ID from [Movie] inner join [Order] on "
+                              + "[Movie].ID = [Order].MovieID where [Order].MovieGoerEmail = @CheckOut)"
+            */
+
+            //.Select(mo => new Movie { ID = mo.ID, Genre = mo.Genre, Price = mo.Price, ReleaseDate = mo.ReleaseDate, Title = mo.Title });
+
+            // Run the query and save the results in DiffMovies for passing to content file
+            SameDates = await sameDates.ToListAsync();
+            // Save the options for both dropdown lists in ViewData for passing to content file
+            //ViewData["MovieGoerList"] = new SelectList(_context.MovieGoer, "Email", "FullName");
+            // invoke the content file
+
+            var dateDiff = BookingInput.CheckOut.Date - BookingInput.CheckIn.Date;
+
+            ViewData["DateDiff"] = dateDiff;
+
+            // creating a subscription object for inserting into database
+            Booking booking = new Booking();
+            booking.RoomID = BookingInput.RoomID;
+            booking.CheckIn = BookingInput.CheckIn;
+            booking.CheckOut = BookingInput.CheckOut;
+            booking.CustomerEmail = customer.Email;
+
+            // retrieve the ordered magazine to get its price
+            var theRoom = await _context.Room.FindAsync(BookingInput.RoomID);
+
+            Room room = new Room();
+
+            // calculate the total price of this order **MAKE CHANGES TO CONVERT THE DATEDIFF TO INTEGER**
+            booking.Cost = dateDiff * theRoom.Price;
+            
+
+            ViewData["TotalPrice"] = booking.Cost;
+
+            _context.Booking.Add(booking);
             await _context.SaveChangesAsync();
 
-            return RedirectToPage("./Index");
+
+
+            return Page();
         }
     }
 }
